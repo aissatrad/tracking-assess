b555992036106d3cf61aad92806803460d0ce5d5
Aissa TRAD <aissa.trad@fst.utm.tn>
2022-02-13 13:57:36 +0100

    app developer api 1.2

--- controller.py
+++ controller.py
@@ -2,7 +2,11 @@ from google_play_scraper import app
 
 
 def get_developer(app_id: str):
-    return app(app_id).get("developerId")
-
-
-
+    result: str
+    try:
+        rs = app(app_id)
+        result = rs.get("developerId")
+    except Exception as e:
+        result = "Unknown"
+        print(e)
+    return result
