e33ade1dd3be7f07d6589327253c32255a07a37a
Aissa TRAD <aissa.trad@fst.utm.tn>
2022-02-13 13:51:00 +0100

    app developer api 1.1

--- controller.py
+++ controller.py
@@ -1,6 +1,8 @@
 from google_play_scraper import app
 
 
-async def get_developer(app_id: str):
-    result = await app(app_id)
-    return result.get("developerId")
+def get_developer(app_id: str):
+    return app(app_id).get("developerId")
+
+
+
